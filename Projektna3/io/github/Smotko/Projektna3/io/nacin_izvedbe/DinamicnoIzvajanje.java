86351ce44f108ed0da76018f5432a64a39364338
Smotko <114558526+Smotko@users.noreply.github.com>
2023-06-19 14:22:11 +0200

    Add files via upload

---
+++ Projektna3/io/github/Smotko/Projektna3/io/nacin_izvedbe/DinamicnoIzvajanje.java
@@ -0,0 +1,31 @@
+package io.github.Smotko.Projektna3.io.nacin_izvedbe;
+
+import java.io.*;
+import java.net.*;
+
+public class DinamicnoIzvajanje {
+    static boolean dosegljivost = true;
+
+    public static void posljiProsnjoPing(String ipNaslov) throws UnknownHostException, IOException {
+        InetAddress naslov = InetAddress.getByName(ipNaslov);
+        System.out.print("Posiljanje prosnje Ping " + ipNaslov);
+
+        if (naslov.isReachable(500)) {
+            System.out.println(" Gostitelj je dosegljiv.");
+            dosegljivost = true;
+        }else {
+            System.out.println(" Oprostite, tega gostitelja ne moram doseci!");
+            dosegljivost = false;
+        }
+    }
+
+    public static String getDosegljivost() {
+        if (dosegljivost == true) {
+            return "dosegljiv";
+        }else  {
+            return "nedosegljiv";
+        }
+    }
+
+
+}
